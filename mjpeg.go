ebd6b5b2a2836243cab6c2ee8b4898d58b20593e
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2016-11-04 00:55:53 +0900

    go vet && golint

--- mjpeg.go
+++ mjpeg.go
@@ -11,17 +11,20 @@ import (
 	"sync"
 )
 
+// Decoder decode motion jpeg
 type Decoder struct {
 	r *multipart.Reader
 	m sync.Mutex
 }
 
+// NewDecoder return new instance of Decoder
 func NewDecoder(r io.Reader, b string) *Decoder {
 	d := new(Decoder)
 	d.r = multipart.NewReader(r, b)
 	return d
 }
 
+// NewDecoderFromResponse return new instance of Decoder from http.Response
 func NewDecoderFromResponse(res *http.Response) (*Decoder, error) {
 	_, param, err := mime.ParseMediaType(res.Header.Get("Content-Type"))
 	if err != nil {
@@ -30,6 +33,7 @@ func NewDecoderFromResponse(res *http.Response) (*Decoder, error) {
 	return NewDecoder(res.Body, strings.Trim(param["boundary"], "-")), nil
 }
 
+// NewDecoderFromURL return new instance of Decoder from response which specified URL
 func NewDecoderFromURL(u string) (*Decoder, error) {
 	req, err := http.NewRequest("GET", u, nil)
 	if err != nil {
@@ -42,6 +46,7 @@ func NewDecoderFromURL(u string) (*Decoder, error) {
 	return NewDecoderFromResponse(res)
 }
 
+// Decode do decoding
 func (d *Decoder) Decode() (image.Image, error) {
 	p, err := d.r.NextPart()
 	if err != nil {
