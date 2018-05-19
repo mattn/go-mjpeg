96f6ed8345058fceea102dcdc4deeeae32ebfbab
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2018-05-19 21:45:40 +0900

    add Stream

--- mjpeg.go
+++ mjpeg.go
@@ -1,14 +1,17 @@
 package mjpeg
 
 import (
+	"fmt"
 	"image"
 	"image/jpeg"
 	"io"
 	"mime"
 	"mime/multipart"
 	"net/http"
+	"net/textproto"
 	"strings"
 	"sync"
+	"time"
 )
 
 // Decoder decode motion jpeg
@@ -53,4 +56,79 @@ func (d *Decoder) Decode() (image.Image, error) {
 		return nil, err
 	}
 	return jpeg.Decode(p)
+}
+
+type Stream struct {
+	m sync.Mutex
+	s map[chan []byte]struct{}
+}
+
+func NewStream() *Stream {
+	return &Stream{
+		s: make(map[chan []byte]struct{}),
+	}
+}
+
+func (s *Stream) Close() error {
+	s.m.Lock()
+	defer s.m.Unlock()
+	for c := range s.s {
+		close(c)
+		delete(s.s, c)
+	}
+	return nil
+}
+
+func (s *Stream) Update(b []byte) {
+	s.m.Lock()
+	defer s.m.Unlock()
+	for c := range s.s {
+		select {
+		case c <- b:
+		default:
+		}
+	}
+}
+
+func (s *Stream) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	c := make(chan []byte)
+	s.m.Lock()
+	s.s[c] = struct{}{}
+	s.m.Unlock()
+
+	defer func() {
+		close(c)
+		s.m.Lock()
+		delete(s.s, c)
+		s.m.Unlock()
+	}()
+
+	m := multipart.NewWriter(w)
+	defer m.Close()
+
+	w.Header().Set("Content-Type", "multipart/x-mixed-replace; boundary="+m.Boundary())
+	w.Header().Set("Connection", "close")
+	h := textproto.MIMEHeader{}
+	st := fmt.Sprint(time.Now().Unix())
+	for {
+		b, ok := <-c
+		if !ok {
+			break
+		}
+		h.Set("Content-Type", "image/jpeg")
+		h.Set("Content-Length", fmt.Sprint(len(b)))
+		h.Set("X-StartTime", st)
+		h.Set("X-TimeStamp", fmt.Sprint(time.Now().Unix()))
+		mw, err := m.CreatePart(h)
+		if err != nil {
+			break
+		}
+		_, err = mw.Write(b)
+		if err != nil {
+			break
+		}
+		if flusher, ok := mw.(http.Flusher); ok {
+			flusher.Flush()
+		}
+	}
 }
