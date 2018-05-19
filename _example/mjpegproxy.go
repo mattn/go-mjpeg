9baf0a96e2c8d6292cd2b5d0a39922c52a0891a8
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2018-05-19 23:02:23 +0900

    refactoring

--- _example/mjpegproxy.go
+++ _example/mjpegproxy.go
@@ -11,12 +11,43 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"sync"
+	"time"
 
 	"github.com/mattn/go-mjpeg"
 )
 
-var url = flag.String("url", "", "Camera host")
-var addr = flag.String("addr", ":8080", "Server address")
+var (
+	url      = flag.String("url", "", "Camera host")
+	addr     = flag.String("addr", ":8080", "Server address")
+	interval = flag.Duration("interval", 200*time.Millisecond, "interval")
+)
+
+func proxy(wg *sync.WaitGroup, stream *mjpeg.Stream) {
+	defer wg.Done()
+
+	dec, err := mjpeg.NewDecoderFromURL(*url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var buf bytes.Buffer
+	for {
+		img, err := dec.Decode()
+		if err != nil {
+			break
+		}
+		buf.Reset()
+		err = jpeg.Encode(&buf, img, nil)
+		if err != nil {
+			break
+		}
+		err = stream.Update(buf.Bytes())
+		if err != nil {
+			break
+		}
+	}
+}
 
 func main() {
 	flag.Parse()
@@ -25,36 +56,11 @@ func main() {
 		os.Exit(1)
 	}
 
-	dec, err := mjpeg.NewDecoderFromURL(*url)
-	if err != nil {
-		log.Fatal(err)
-	}
+	stream := mjpeg.NewStreamWithInterval(*interval)
 
-	server := &http.Server{Addr: *addr}
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, os.Interrupt)
-	go func() {
-		<-sc
-		server.Shutdown(context.Background())
-	}()
-
-	stream := mjpeg.NewStream()
-
-	go func() {
-		var buf bytes.Buffer
-		for {
-			img, err := dec.Decode()
-			if err != nil {
-				break
-			}
-			buf.Reset()
-			err = jpeg.Encode(&buf, img, nil)
-			if err != nil {
-				break
-			}
-			stream.Update(buf.Bytes())
-		}
-	}()
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go proxy(&wg, stream)
 
 	http.HandleFunc("/jpeg", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "image/jpeg")
@@ -68,5 +74,15 @@ func main() {
 		w.Write([]byte(`<img src="/mjpeg" />`))
 	})
 
+	server := &http.Server{Addr: *addr}
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, os.Interrupt)
+	go func() {
+		<-sc
+		server.Shutdown(context.Background())
+	}()
 	server.ListenAndServe()
+	stream.Close()
+
+	wg.Wait()
 }
